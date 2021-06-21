14f726d79b1bcc3b5cf7c071f66880ab56ad170f
masopego <masopego@gmail.com>
2021-06-21 21:53:11 +0200

    Add create book view

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
 const port = 3000;
 
 const booksRouter = require("./routes/books");
@@ -9,7 +11,7 @@ const db = require("./models/index");
 db.sequelize.sync();
 
 app.use(express.json());
-
+app.use(cors());
 app.use(booksRouter);
 app.use(authorsRouter);
 
